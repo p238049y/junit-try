ef6eff1a675653a05fe762db8783e20ab7a3688c
p238049y <p238049y@gmail.com>
2024-07-06 01:35:54 +0900

    first commit

---
+++ src/main/java/com/p238049y/junit_app/JunitAppApplication.java
@@ -0,0 +1,13 @@
+package com.p238049y.junit_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p238049y/junit_app/JunitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p238049y.junit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
