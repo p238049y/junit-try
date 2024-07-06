be3b646389850dc10746d5bc3f99d1c382139a3e
p238049y <p238049y@gmail.com>
2024-07-06 20:37:22 +0900

    [add] #2 PhoneNumberとMailAddressのどちらか一方の値がnullであればtrueを返すかを判定するhasEitherPhoneNumberOrMailAddressのテストコードを追加

--- src/test/java/com/p238049y/junit_app/presentation/controller/post/PostUserDataRequestTest.java
+++ src/test/java/com/p238049y/junit_app/presentation/controller/post/PostUserDataRequestTest.java
@@ -5,18 +5,115 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 public class PostUserDataRequestTest {
+    private static final BigInteger ID = BigInteger.ONE;
+    private static final String NAME = "パイロットユーザー";
+    private static final String PHONE_NUMBER = "090-1234-5678";
+    private static final String MAIL_ADDRESS = "A@example.com";
 
     @Nested
-    @DisplayName("ハッピーテスト")
-    class HappyTest {
+    @DisplayName("PhoneNumberとMailAddressのどちらか一方の値がnullであればtrueを返すかを判定するhasEitherPhoneNumberOrMailAddressOldのテスト")
+    class HasEitherPhoneNumberOrMailAddressOld {
+        @Test
+        void PhoneNumberのみnullの場合trueを返すこと () {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    null,
+                    MAIL_ADDRESS
+            );
+
+            Assertions.assertTrue(request.hasEitherPhoneNumberOrMailAddressOld());
+        }
 
         @Test
-        void _１と１をを足すと２になること () {
-            var actual = 1+1;
-            var expect = 2;
+        void MailAddressのみnullの場合trueを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    PHONE_NUMBER,
+                    null
+            );
 
-            Assertions.assertEquals(actual, expect);
+            Assertions.assertTrue(request.hasEitherPhoneNumberOrMailAddressOld());
+        }
+
+        @Test
+        void MailAddressとPhoneNumberの両方の値がnullの場合falseを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    null,
+                    null
+            );
+
+            Assertions.assertFalse(request.hasEitherPhoneNumberOrMailAddressOld());
+        }
+
+        @Test
+        void MailAddressとPhoneNumberの両方の値が存在する場合falseを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    PHONE_NUMBER,
+                    MAIL_ADDRESS
+            );
+
+            Assertions.assertFalse(request.hasEitherPhoneNumberOrMailAddressOld());
+        }
+    }
+
+    @Nested
+    @DisplayName("PhoneNumberとMailAddressのどちらか一方の値がnullであればtrueを返すかを判定するhasEitherPhoneNumberOrMailAddressNewのテスト")
+    class HasEitherPhoneNumberOrMailAddressNew {
+        @Test
+        void PhoneNumberのみnullの場合trueを返すこと () {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    null,
+                    MAIL_ADDRESS
+            );
+
+            Assertions.assertTrue(request.hasEitherPhoneNumberOrMailAddressNew());
+        }
+
+        @Test
+        void MailAddressのみnullの場合trueを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    PHONE_NUMBER,
+                    null
+            );
+
+            Assertions.assertTrue(request.hasEitherPhoneNumberOrMailAddressNew());
+        }
+
+        @Test
+        void MailAddressとPhoneNumberの両方の値がnullの場合falseを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    null,
+                    null
+            );
+
+            Assertions.assertFalse(request.hasEitherPhoneNumberOrMailAddressNew());
+        }
+
+        @Test
+        void MailAddressとPhoneNumberの両方の値が存在する場合falseを返すこと() {
+            var request = new PostUserDataRequest(
+                    ID,
+                    NAME,
+                    PHONE_NUMBER,
+                    MAIL_ADDRESS
+            );
+
+            Assertions.assertFalse(request.hasEitherPhoneNumberOrMailAddressNew());
         }
     }
 }
